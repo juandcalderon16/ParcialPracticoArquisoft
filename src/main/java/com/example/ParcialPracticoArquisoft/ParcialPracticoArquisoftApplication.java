2eaf123b41d29da0ff990c2354b920faed4aa96d
juandcalderon16 <juand.calderon@udea.edu.co>
2024-11-29 10:12:27 -0500

    Primer commit

---
+++ src/main/java/com/example/ParcialPracticoArquisoft/ParcialPracticoArquisoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParcialPracticoArquisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialPracticoArquisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialPracticoArquisoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParcialPracticoArquisoft/ParcialPracticoArquisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParcialPracticoArquisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialPracticoArquisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
